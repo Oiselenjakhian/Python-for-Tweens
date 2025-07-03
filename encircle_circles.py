879437265865befc1e5a3860d1f23c65ee6a2a21
Truston Ailende <trustonailende@gmail.com>
2025-07-03 10:42:20 +0100

    Add files via upload

---
+++ encircle_circles.py
@@ -0,0 +1,107 @@
+"""
+Developer Name: Truston Ailende
+Email Address: trustonailende@gmail.com
+"""
+import turtle
+import math
+ 
+# Square
+def drawSquare(length):
+    turtle.penup()
+    turtle.setposition(-length/2.0, length/2.0)
+    turtle.pendown()
+    for i in range(0, 4):
+        turtle.forward(length)
+        turtle.right(90)
+    turtle.penup()
+    turtle.home()
+ 
+# Horizontal lines
+def drawHorizontalLine(length, division):
+    pixelSpace = int(length / division)
+    half = int(length / 2)
+    for j in range((-half + pixelSpace), half, pixelSpace):
+        turtle.penup()
+        turtle.setposition(-half, j)
+        turtle.pendown()
+        turtle.forward(length)
+    turtle.penup()
+    turtle.home()
+ 
+# Vertical lines
+def drawVerticalLine(length, division):
+    pixelSpace = int(length / division)
+    half = int(length / 2)
+    turtle.right(90)
+    for k in range((-half + pixelSpace), half, pixelSpace):
+        turtle.penup()
+        turtle.setposition(k, half)
+        turtle.pendown()
+        turtle.forward(length)
+    turtle.penup()
+    turtle.home()
+ 
+# Draw the grid
+turtle.speed(1000000)
+drawSquare(400)
+drawHorizontalLine(400, 40)
+drawVerticalLine(400, 40)
+ 
+# Change the colour mode
+turtle.colormode(255)
+ 
+# Change the pencolor to red
+turtle.pencolor(255, 0, 0)
+ 
+# Draw the horizontal centre line
+turtle.setposition(-200, 0)
+turtle.pendown()
+turtle.forward(400)
+turtle.penup()
+ 
+# Draw the vertical centre line
+turtle.setposition(0, 200)
+turtle.setheading(270)
+turtle.pendown()
+turtle.forward(400)
+ 
+# Reset all the properties
+turtle.home()
+turtle.pencolor(0, 0, 0)
+ 
+# Place code here
+turtle.penup()
+turtle.setposition(0, -25)
+turtle.pendown()
+turtle.pensize(10)
+turtle.circle(50)
+
+turtle.penup()
+turtle.setposition(25, 0)
+turtle.setheading(90)
+turtle.pendown()
+turtle.pensize(10)
+turtle.circle(50)
+
+turtle.penup()
+turtle.setposition(0, 25)
+turtle.setheading(180)
+turtle.pendown()
+turtle.pensize(10)
+turtle.circle(50)
+
+turtle.penup()
+turtle.setposition(-25, 0)
+turtle.setheading(270)
+turtle.pendown()
+turtle.pensize(10)
+turtle.circle(50)
+
+turtle.penup()
+turtle.setposition(75, 0)
+turtle.setheading(90)
+turtle.pendown()
+turtle.circle(75)
+
+# End the program
+turtle.done()
