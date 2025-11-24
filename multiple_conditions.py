a73c3c83daaaa962a51cd59b76d7c2730682f1c6
Truston Ailende <trustonailende@gmail.com>
2025-11-24 17:22:52 +0100

    Add files via upload

---
+++ multiple_conditions.py
@@ -0,0 +1,94 @@
+"""
+Developer Name: Truston Ailende
+Email Address: trustonailende@gmail.com
+"""
+import turtle
+import math
+ 
+# Square
+def drawSquare(length):
+    turtle.penup()
+    turtle.setposition(-length/2.0, length/2.0)
+    turtle.pendown()
+    for i in range(0, 4):
+        turtle.forward(length)
+        turtle.right(90)
+    turtle.penup()
+    turtle.home()
+ 
+# Horizontal lines
+def drawHorizontalLine(length, division):
+    pixelSpace = int(length / division)
+    half = int(length / 2)
+    for j in range((-half + pixelSpace), half, pixelSpace):
+        turtle.penup()
+        turtle.setposition(-half, j)
+        turtle.pendown()
+        turtle.forward(length)
+    turtle.penup()
+    turtle.home()
+ 
+# Vertical lines
+def drawVerticalLine(length, division):
+    pixelSpace = int(length / division)
+    half = int(length / 2)
+    turtle.right(90)
+    for k in range((-half + pixelSpace), half, pixelSpace):
+        turtle.penup()
+        turtle.setposition(k, half)
+        turtle.pendown()
+        turtle.forward(length)
+    turtle.penup()
+    turtle.home()
+ 
+# Draw the grid
+turtle.speed(1000000)
+drawSquare(400)
+drawHorizontalLine(400, 40)
+drawVerticalLine(400, 40)
+ 
+# Change the colour mode
+turtle.colormode(255)
+ 
+# Change the pencolor to red
+turtle.pencolor(255, 0, 0)
+ 
+# Draw the horizontal centre line
+turtle.setposition(-200, 0)
+turtle.pendown()
+turtle.forward(400)
+turtle.penup()
+ 
+# Draw the vertical centre line
+turtle.setposition(0, 200)
+turtle.setheading(270)
+turtle.pendown()
+turtle.forward(400)
+ 
+# Reset all the properties
+turtle.home()
+turtle.pencolor(0, 0, 0)
+ 
+# Place code here
+# Set the style for the pen
+turtle.color('red')
+
+# Set the style
+style = ('Courier Bold', 20, 'italic')
+
+# Ask the user for input
+user_input = int(turtle.textinput("Enter a Number", "Enter a number from 1 to 4"))
+
+if (user_input == 1):
+    turtle.write("You entered the number 1", font=style, align='center')
+elif (user_input == 2):
+    turtle.write("You entered the number 2", font=style, align='center')
+elif (user_input == 3):
+    turtle.write("You entered the number 3", font=style, align='center')
+elif (user_input == 4):
+    turtle.write("You entered the number 4", font=style, align='center')
+else:
+    turtle.write("Invalid input!", font=style, align='center')
+
+# End the program
+turtle.done()
