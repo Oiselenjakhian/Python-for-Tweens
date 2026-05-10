048e7ebecc83efdba574a1646fe2d9addb706948
Truston Ailende <trustonailende@gmail.com>
2026-05-10 20:13:17 +0100

    Add files via upload

---
+++ colour_picker.py
@@ -0,0 +1,95 @@
+"""
+Developer Name: Truston Ailende
+Email Address: trustonailende@gmail.com
+"""
+import turtle
+import math
+ 
+# Square
+def drawSquare(length):
+    turtle.penup()
+    turtle.setposition(-length/2.0, length/2.0)
+    turtle.pendown()
+    for i in range(0, 4):
+        turtle.forward(length)
+        turtle.right(90)
+    turtle.penup()
+    turtle.home()
+ 
+# Horizontal lines
+def drawHorizontalLine(length, division):
+    pixelSpace = int(length / division)
+    half = int(length / 2)
+    for j in range((-half + pixelSpace), half, pixelSpace):
+        turtle.penup()
+        turtle.setposition(-half, j)
+        turtle.pendown()
+        turtle.forward(length)
+    turtle.penup()
+    turtle.home()
+ 
+# Vertical lines
+def drawVerticalLine(length, division):
+    pixelSpace = int(length / division)
+    half = int(length / 2)
+    turtle.right(90)
+    for k in range((-half + pixelSpace), half, pixelSpace):
+        turtle.penup()
+        turtle.setposition(k, half)
+        turtle.pendown()
+        turtle.forward(length)
+    turtle.penup()
+    turtle.home()
+ 
+# Draw the grid
+turtle.speed(1000000)
+drawSquare(400)
+drawHorizontalLine(400, 40)
+drawVerticalLine(400, 40)
+ 
+# Change the colour mode
+turtle.colormode(255)
+ 
+# Change the pencolor to red
+turtle.pencolor(255, 0, 0)
+ 
+# Draw the horizontal centre line
+turtle.setposition(-200, 0)
+turtle.pendown()
+turtle.forward(400)
+turtle.penup()
+ 
+# Draw the vertical centre line
+turtle.setposition(0, 200)
+turtle.setheading(270)
+turtle.pendown()
+turtle.forward(400)
+ 
+# Reset all the properties
+turtle.home()
+turtle.pencolor(0, 0, 0)
+ 
+# Place code here
+# Set the style for the pen
+turtle.color('red')
+
+# Ask the user for input
+user_input = int(turtle.textinput("Enter a Number",
+                "Enter a number from 1 to 4: \n1. Red\n2. Green\n3. Blue\n4. Yellow"))
+
+if (user_input == 1):
+    turtle.dot(30, "red")
+elif (user_input == 2):
+    turtle.dot(30, "green")
+elif (user_input == 3):
+    turtle.dot(30, "blue")
+elif (user_input == 4):
+    turtle.dot(30, "yellow")
+else:
+    turtle.write("Invalid input!", font=style, align='center')
+
+# Hide the turtle
+turtle.hideturtle()
+ 
+# End the program
+turtle.done()
